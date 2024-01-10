9ae57e283fcb49d3292f62eadcccda1f86a77af4
satuhark <148077002+satuhark@users.noreply.github.com>
2024-01-10 10:51:13 +0200

    Update main.jsx

--- osa1/Kurssitiedot /main.jsx
+++ osa1/Kurssitiedot /main.jsx
@@ -1,12 +1,4 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const root = document.getElementById('root');
-
-const reactRoot = createRoot(root);
-reactRoot.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
